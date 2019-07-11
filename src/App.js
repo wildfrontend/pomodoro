5c018fab0263ad3e4be4a7eca78fdcce650bdc06
Louis Luo <louis.luo@alliance.tw>
2019-07-11 16:37:35 +0800

    移轉Navbar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React from 'react'
 
 import { ThemeProvider } from 'styled-components'
-import { Box, Flex, Text } from 'rebass'
+import { Box, Flex } from 'rebass'
 import { theme } from './styles/theme'
 
-import { FiList, FiBarChart, FiMusic } from 'react-icons/fi'
-
 import './App.scss'
+
+import { Navbar } from './components/Navbar'
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
             <Flex className='app'>
-                <Box  width={1 / 12} color='magenta' bg='#FF7575'>
-                    <Box p={4} fontSize={4} color='white' bg='#CB2424'>
-                        <FiList size='3rem' />
-                    </Box>
-                    <Box p={4} fontSize={4} color='white' bg='#E54343'>
-                        <FiBarChart size='3rem' />
-                    </Box>
-                    <Box p={4} fontSize={4} color='white' bg='#F85A5A'>
-                        <FiMusic size='3rem' />
-                    </Box>
-
-                    <Text
-                        color='white '
-                        fontSize={6}
-                        callName='logo-name'
-                        className='nav-logoname'
-                    >
-                        Pomotoro
-                    </Text>
+                <Box width={1 / 12} color='magenta' bg='#FF7575'>
+                    <Navbar />
                 </Box>
                 <Box p={3} width={5 / 12} color='white' bg='white'>
                     Box
