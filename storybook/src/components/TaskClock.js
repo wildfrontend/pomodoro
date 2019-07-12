ccaca47710d39e0c145f31dc643b1218667c21d8
Louis Luo <louis.luo@alliance.tw>
2019-07-12 17:24:39 +0800

    完成倒數功能

--- storybook/src/components/TaskClock.js
+++ storybook/src/components/TaskClock.js
@@ -1,21 +1,51 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { FaStopCircle, FaTimes, FaPlay } from 'react-icons/fa'
 
 import './TaskClock.scss'
 
 export const TaskClock = props => {
-    const [pause, setPause] = useState(false)
+    const [pause, setPause] = useState(true)
+    const [seconds, setSeconds] = useState(25 * 60)
+    useEffect(() => {
+        const int = setInterval(() => {
+            console.log(`${Date.now()} - pause: ${pause}`)
+            if (!pause) {
+                setSeconds(s => s - 1)
+            }
+        }, 1000)
+        return () => {
+            clearInterval(int)
+        }
+    }, [pause])
+
+    const startTimer = () => {
+        setPause(false)
+    }
+    const pauseTimer = () => {
+        setPause(true)
+    }
+    const resetTimer = () => {
+        setPause(true)
+        setSeconds(25 * 60)
+    }
     return (
         <Fragment>
             <div className='task-clock'>
                 <div className='clock-info'>
-                    <span className='reciprocal'>12:35</span>
+                    <span className='reciprocal'>{`${Math.floor(
+                        seconds / 60
+                    )}:${('00' + (seconds % 60)).slice(-2)}`}</span>
                     <div className='control'>
                         {pause ? (
+                            <div onClick={() => startTimer()}>
+                                <span>開始</span>
+                                <FaPlay />
+                            </div>
+                        ) : (
                             <>
                                 <div
                                     className='left'
-                                    onClick={() => setPause(false)}
+                                    onClick={() => pauseTimer()}
                                 >
                                     <FaStopCircle />
                                     <span>暫停</span>
@@ -25,11 +55,6 @@ export const TaskClock = props => {
                                     <span>作廢</span>
                                 </div>
                             </>
-                        ) : (
-                            <div onClick={() => setPause(true)}>
-                                <span>開始</span>
-                                <FaPlay />
-                            </div>
                         )}
                     </div>
                 </div>
