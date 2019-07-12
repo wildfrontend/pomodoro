af5b74753853c1a877328da7b9647f78112e735e
Louis Luo <louis.luo@alliance.tw>
2019-07-12 14:22:36 +0800

    新增 Teaxarea

--- storybook/src/stories/index.js
+++ storybook/src/stories/index.js
@@ -5,10 +5,14 @@ import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 
 import { Welcome } from '@storybook/react/demo'
+
 import { ProcessTask } from '../components/ProcessTask'
+import { CreateTask } from '../components/CreateTask'
 
 storiesOf('Welcome', module).add('to Storybook', () => (
     <Welcome showApp={linkTo('Button')} />
 ))
 
-storiesOf('Task', module).add('Todolist', () => <ProcessTask />)
+storiesOf('Task', module)
+    .add('Todolist', () => <ProcessTask />)
+    .add('Create Task', () => <CreateTask />)
